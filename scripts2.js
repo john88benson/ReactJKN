63f3958e4096a0b295f0a25255e0d79b2a41863c
Benson, John <jbenson@withum.com>
2017-06-15 13:16:24 -0400

    Benson 061517 JKN Tut Stage 4

--- scripts2.js
+++ scripts2.js
@@ -17,24 +17,36 @@ var ContactItem = React.createClass({
 
 var ContactForm = React.createClass({
     propTypes: {
-        contact: React.PropTypes.object.isRequired
+        value: React.PropTypes.object.isRequired,
+        onChange: React.PropTypes.func.isRequired,
     },
     render: function(){
+        var oldContact = this.props.value;
+        var onChange = this.props.onChange;
         return(
             React.createElement('form', {className: 'ContactForm'},
                 React.createElement('input',{
                     type: 'text',
                     placeholder: 'Name (required)',
-                    value: this.props.contact.name,
+                    value: this.props.value.name,
+                    onChange: function(e){
+                        onChange(Object.assign({}, oldContact, {name: e.target.value}));
+                    }
                 }),
                 React.createElement('input',{
                     type: 'email',
                     placeholder: 'Email(required)',
-                    value: this.props.contact.email,
+                    value: this.props.value.email,
+                    onChange: function(e){
+                        onChange(Object.assign({}, oldContact, {email: e.target.value}));
+                    }
                 }),
                 React.createElement('textarea',{
                     placeholder: 'Description',
-                    value: this.props.contact.description,
+                    value: this.props.value.description,
+                    onChange: function(e){
+                        onChange(Object.assign({}, oldContact, {description: e.target.value}));
+                    }
                 }),
                 React.createElement('button', {type: 'submit'}, "Add Contact")
             )
@@ -46,6 +58,7 @@ var ContactView = React.createClass({
     propTypes:{
         contacts: React.PropTypes.array.isRequired,
         newContact: React.PropTypes.object.isRequired,
+        onNewContactChange: React.PropTypes.func.isRequired,
     },
     render: function(){
         var contactItemElements = this.props.contacts
@@ -55,25 +68,37 @@ var ContactView = React.createClass({
             React.createElement('div', {className: 'ContactView'},
                 React.createElement('h1', {className: 'ContactView-title'}, "Contacts"),
                 React.createElement('ul', {className: 'ContactView-list'}, contactItemElements),
-                React.createElement(ContactForm, {contact: this.props.newContact})
+                React.createElement(ContactForm, {
+                    value: this.props.newContact,
+                    onChange: this.props.onNewContactChange,
+                })
             )
-        )
-    }
-})
+        );
+    },
+});
 
-var contacts = [
-    {key: 1, name: "John C Benson", email: "jbenson@withum.com", description: "Just Another Developer"},
-    {key: 2, name: "Robert A Shaw", email: "rshaw@example.com"},
-    {key: 3, name: "Bobby Tables"},
-    {key: 4, name: "Isaac Asimov", email: "iasimov@trantor.gov", description: "One of the Big Three"}
-]
+function updateNewContact(contact){
+    setState({ newContact: contact})
+}
 
-var newContact = {name: "", email: "", description: ""}
+var state = {};
 
-ReactDOM.render(
-    React.createElement(ContactView,{
-        contacts: contacts,
-        newContact: newContact
-    })
-    , document.getElementById('react-app')
-)
+function setState(changes){
+    Object.assign(state, changes);
+
+    ReactDOM.render(
+        React.createElement(ContactView, Object.assign({}, state, {onNewContactChange: updateNewContact,})),
+        document.getElementById('react-app')
+    )
+}
+
+setState ({
+    contacts: [
+        {key: 1, name: "John C Benson", email: "jbenson@withum.com", description: "Just Another Developer"},
+        {key: 2, name: "Robert A Shaw", email: "rshaw@example.com"},
+        {key: 3, name: "Bobby Tables"},
+        {key: 4, name: "Isaac Asimov", email: "iasimov@trantor.gov", description: "One of the Big Three"}
+    ],
+    newContact: {name: "", email: "", description: ""},
+});
+
